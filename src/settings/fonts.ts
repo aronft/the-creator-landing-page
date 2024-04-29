ba6fa0e1f1d2e91e4f4c66c97a0a8e53ec5ffea6
Aron <blazte.aster@gmail.com>
2024-04-29 16:31:21 -0500

    Config tailwind colors, fonts with next/fonts, and create Colors story. (#3)

---
+++ src/settings/fonts.ts
@@ -0,0 +1,8 @@
+import { Montserrat } from 'next/font/google'
+
+export const font = Montserrat({
+    weight: ['400', '700', '500', '600'],
+    display: 'swap',
+    variable: '--font-montserrat',
+    subsets: ['latin'],
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['var(--font-montserrat)'],
+            },
+        },
+        colors: {
+            primary: '#FF60E6',
+            'primary-alt': '#FFADF2',
+            secondary: '#1F392C',
+            tertiary: '#F8F5F0',
+        },
+        fontSize: {
+            base: '1rem',
+            lg: '1.125rem',
+            xl: '1.25rem',
+            '2xl': '1.5rem',
+            '3xl': '2rem',
+            '4xl': '2.5rem',
+            '5xl': '3rem',
+            '6xl': '3.5rem',
+            '9xl': '7rem',
+        },
+        fontWeight: {
+            normal: '400',
+            medium: '500',
+            semibold: '600',
+            bold: '700',
+        },
     },
-  },
-  plugins: [],
-};
-export default config;
+    plugins: [],
+}
+export default config
