188be6ee8cd3e0a40f80056245070af830c11e8d
Aron <blazte.aster@gmail.com>
2024-05-02 15:38:01 -0500

    fix: replace clip-path for clipPath (#17)

--- src/components/icons/pinterest.tsx
+++ src/components/icons/pinterest.tsx
@@ -10,7 +10,7 @@ export const Pinterest = ({ ...props }: SvgProps) => {
             xmlns="http://www.w3.org/2000/svg"
             {...props}
         >
-            <g clip-path="url(#clip0_54_37)">
+            <g clipPath="url(#clip0_54_37)">
                 <path
                     d="M20 40C31.0457 40 40 31.0457 40 20C40 8.9543 31.0457 0 20 0C8.9543 0 0 8.9543 0 20C0 31.0457 8.9543 40 20 40Z"
                     fill="white"
