e9f7394341a70d5d0ec09d6056f4a13d5f9e973c
Aron <blazte.aster@gmail.com>
2024-05-03 18:38:05 -0500

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Footer } from '@/components/sections/footer/footer'
 import { Header } from '@/components/sections/header/header'
+import { Hero } from '@/components/sections/hero/hero'
 import { ReliabilitySection } from '@/components/sections/reliability-section/reliability-section'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
         <>
             <Header />
             <main>
+                <Hero />
                 <ReliabilitySection />
             </main>
             <Footer />
