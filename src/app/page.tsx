f12a254c2938172eb29dfe3149e4a8b10996b249
Aron <blazte.aster@gmail.com>
2024-05-06 08:03:38 -0500

    combine sub sections in how we work section and use into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Footer } from '@/components/sections/footer/footer'
 import { Header } from '@/components/sections/header/header'
 import { Hero } from '@/components/sections/hero/hero'
+import { HowWeWork } from '@/components/sections/how-we-work/how-we-work'
 import { ReliabilitySection } from '@/components/sections/reliability-section/reliability-section'
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
             <Header />
             <main>
                 <Hero />
+                <HowWeWork />
                 <ReliabilitySection />
             </main>
             <Footer />
