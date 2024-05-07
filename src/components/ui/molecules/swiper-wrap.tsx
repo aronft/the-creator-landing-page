316ed663d35b58f3445e3167d0e393b3e6b14436
Aron <blazte.aster@gmail.com>
2024-05-07 15:28:58 -0500

    remove default breakpoints in SwiperWrap component and change display default style for flex

--- src/components/ui/molecules/swiper-wrap.tsx
+++ src/components/ui/molecules/swiper-wrap.tsx
@@ -1,42 +1,35 @@
 'use client'
 
 import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react'
-import { Swiper as SwiperCore } from 'swiper/types'
 
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
 import { cn } from '@/utils/class-names'
-import { Children, useContext, useRef } from 'react'
+import { Children } from 'react'
 import styles from './swiper-wrap.module.css'
 
-interface SwiperWrapProps extends SwiperProps {}
+interface SwiperWrapProps extends SwiperProps {
+    swiperSlideClassName?: string
+}
 
 export const SwiperWrap = ({
     className,
+    swiperSlideClassName,
     children,
     ...props
 }: SwiperWrapProps) => {
-    const swiperRef = useRef<SwiperCore>()
     return (
         <Swiper
             spaceBetween={32}
             slidesPerView={1}
             className={cn('', className)}
             {...props}
-            breakpoints={{
-                768: {
-                    slidesPerView: 2,
-                    spaceBetween: 40,
-                },
-                1024: {
-                    slidesPerView: 3,
-                    spaceBetween: 50,
-                },
-            }}
         >
             {Children.map(children, (child) => (
-                <SwiperSlide className={styles['swiper-slide']}>
+                <SwiperSlide
+                    className={cn(styles['swiper-slide'], swiperSlideClassName)}
+                >
                     {child}
                 </SwiperSlide>
             ))}
