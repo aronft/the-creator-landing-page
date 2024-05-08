fa08cd49528f4607710f8195566fdd0fbbe4f61b
Aron <blazte.aster@gmail.com>
2024-05-08 11:12:05 -0500

    add Menu component

---
+++ src/components/ui/molecules/menu/menu.tsx
@@ -0,0 +1,21 @@
+import { DATA } from '@/constants/data'
+import { UiList } from '../../objects/ui-list/ui-list'
+import { cn } from '@/utils/class-names'
+
+export const Menu = ({ className }: { className?: string }) => {
+    const menu = DATA.menu
+    console.log(className)
+    return (
+        <UiList className={cn('', className)}>
+            {menu.map((item) => (
+                <a
+                    href={`#${item.url}`}
+                    key={item.id}
+                    className="font-medium text-secondary"
+                >
+                    {item.name}
+                </a>
+            ))}
+        </UiList>
+    )
+}
