499d184fc5b1cc5b9de094f037464411d467df08
Aron <blazte.aster@gmail.com>
2024-05-13 15:13:05 -0500

    add animation to svg on hero, partners and how we work sections (#37)

---
+++ src/components/hoc/with-animation.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import useOnEscreen from '@/hooks/use-on-escreen'
+import { cn } from '@/utils/class-names'
+import React, { forwardRef } from 'react'
+
+export function withAnimation(
+    Component: React.ComponentType<{}>,
+    className: string
+) {
+    return function WithAnimation(props: any) {
+        const { ref, isIntersecting } = useOnEscreen({ threshold: 0.5 })
+        console.log(isIntersecting, ref)
+        return (
+            <Component
+                {...props}
+                ref={ref}
+                className={cn(
+                    'opacity-0',
+                    {
+                        'animation-left-line opacity-100 ': isIntersecting,
+                    },
+                    className
+                )}
+            />
+        )
+    }
+}
